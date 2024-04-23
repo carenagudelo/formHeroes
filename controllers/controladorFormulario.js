e88b6b3df09637cdae51c7e66dab851b3d158db7
Caren Agudelo <cagudeloc@cesde.net>
2024-04-23 11:24:20 -0500

    add controller

---
+++ controllers/controladorFormulario.js
@@ -0,0 +1,11 @@
+//scope de las variables de JS
+
+//Almacenando una etiqueta de HTML en una variable de JS
+//Esto con el fin de controlar esa etiqueta desde la lógica de JS
+
+let productNameBox = document.getElementById("name");
+let productPriceBx = document.getElementById("price");
+let productCodeBox = document.getElementById("code");
+let productQuantityBox = document.getElementById("quantity");
+let productTypeBox = document.getElementById("type");
+let butonSubmit = document.getElementById("butonSubmit");
