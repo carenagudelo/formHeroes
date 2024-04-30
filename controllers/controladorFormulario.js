b7ff5f758a92f3e904888830b38f03fa19dc2f53
Caren Agudelo <cagudeloc@cesde.net>
2024-04-30 11:32:04 -0500

    "Add events to save values"

--- controllers/controladorFormulario.js
+++ controllers/controladorFormulario.js
@@ -3,9 +3,23 @@
 //Almacenando una etiqueta de HTML en una variable de JS
 //Esto con el fin de controlar esa etiqueta desde la lógica de JS
 
-let productNameBox = document.getElementById("name");
-let productPriceBx = document.getElementById("price");
-let productCodeBox = document.getElementById("code");
-let productQuantityBox = document.getElementById("quantity");
-let productTypeBox = document.getElementById("type");
+let productIdBox = document.getElementById("idProduct").value;
+let productNameBox = document.getElementById("name").value;
+let productPriceBox = document.getElementById("priceUnit").value;
+let productDescriptionBox = document.getElementById("description").value;
+let productPhotoBox = document.getElementById("photo").value;
+let productReviewBox = document.getElementById("review").value;
+let productBrandBox = document.getElementById("brand").value;
 let butonSubmit = document.getElementById("butonSubmit");
+
+//¿Cómo detectar un evento en JavaScript?
+//1. Una vez tenga una variable que almacena la etiqueta sobre la cual quiero escuchar
+//el evento
+
+/*2. Utilizar la función o método propio de js (PERRO GUARDIAN)*/
+
+butonSubmit.addEventListener("click",function(event){
+    event.preventDefault();
+    //lógica que deseo activar si el evento se detectó con éxito
+    console.log("Acaban de darle clic al formulario")
+})
