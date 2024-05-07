f05658890732e0073fb1833cf9a412234add4700
Caren Agudelo <cagudeloc@cesde.net>
2024-05-07 11:36:02 -0500

    create catch value

--- controllers/controladorFormulario.js
+++ controllers/controladorFormulario.js
@@ -3,13 +3,12 @@
 //Almacenando una etiqueta de HTML en una variable de JS
 //Esto con el fin de controlar esa etiqueta desde la lógica de JS
 
-let productIdBox = document.getElementById("idProduct").value;
-let productNameBox = document.getElementById("name").value;
-let productPriceBox = document.getElementById("priceUnit").value;
-let productDescriptionBox = document.getElementById("description").value;
-let productPhotoBox = document.getElementById("photo").value;
-let productReviewBox = document.getElementById("review").value;
-let productBrandBox = document.getElementById("brand").value;
+let productNameBox = document.getElementById("name");
+let productPriceBox = document.getElementById("priceUnit");
+let productDescriptionBox = document.getElementById("description");
+let productPhotoBox = document.getElementById("photo");
+let productReviewBox = document.getElementById("review");
+let productBrandBox = document.getElementById("brand");
 let butonSubmit = document.getElementById("butonSubmit");
 
 //¿Cómo detectar un evento en JavaScript?
@@ -22,4 +21,16 @@ butonSubmit.addEventListener("click",function(event){
     event.preventDefault();
     //lógica que deseo activar si el evento se detectó con éxito
     console.log("Acaban de darle clic al formulario")
+
+    //Como capturar el valor de un input del formulario
+    let nameProdut = productNameBox.value;
+    let priceProduct = productPriceBox.value;
+    let descriptionProduct = productDescriptionBox.value;
+    let photoProduct = productPhotoBox.value; 
+    let reviewProduct = productReviewBox.value;
+    let brandProduct = productBrandBox.value;
+
+    //mostrando los valores capturados
+
+    console.log(nameProdut, priceProduct, descriptionProduct, photoProduct, reviewProduct, brandProduct)
 })
